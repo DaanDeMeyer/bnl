488f1b30810779e19e74fce5b4e38ebe00acdafb
Daan De Meyer <daan.j.demeyer@gmail.com>
2019-04-24 17:01:00 +0200

    Remove prefix from error strings.

--- h3c/src/error.c
+++ h3c/src/error.c
@@ -4,25 +4,25 @@ const char *h3c_strerror(H3C_ERROR error)
 {
   switch (error) {
     case H3C_SUCCESS:
-      return "h3c: success";
+      return "success";
 
       // HTTP/3 connection errors
 
     case H3C_ERROR_MALFORMED_FRAME:
-      return "HTTP/3: malformed frame";
+      return "malformed frame";
 
       // h3c library errors
 
     case H3C_ERROR_INTERNAL:
-      return "h3c: internal error";
+      return "internal error";
     case H3C_ERROR_BUF_TOO_SMALL:
-      return "h3c: buf too small";
+      return "buf too small";
     case H3C_ERROR_VARINT_OVERFLOW:
-      return "h3c: varint overflow";
+      return "varint overflow";
     case H3C_ERROR_SETTING_OVERFLOW:
-      return "h3c: setting overflow";
+      return "setting overflow";
     case H3C_ERROR_INCOMPLETE_FRAME:
-      return "h3c: frame incomplete";
+      return "frame incomplete";
   }
 
   return "unknown error";
