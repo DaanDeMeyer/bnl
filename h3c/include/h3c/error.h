c18278d24c2f2204e8f2c22fce6d0fb636910a57
Daan De Meyer <daan.j.demeyer@gmail.com>
2019-04-24 14:10:55 +0200

    Renumber H3C_ERROR values.
    The first 16 bytes are reserved for HTTP/3 protocol errors which all fit
    within 16 bytes. All numbers larger than 16 bytes are reserved for
    library errors.

--- h3c/include/h3c/error.h
+++ h3c/include/h3c/error.h
@@ -2,18 +2,31 @@
 
 #include <h3c/export.h>
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef enum {
   H3C_SUCCESS = 0,
-  H3C_ERROR_INTERNAL = 1,
-  H3C_ERROR_BUF_TOO_SMALL = 2,
-  H3C_ERROR_VARINT_OVERFLOW = 3,
-  H3C_ERROR_SETTING_OVERFLOW = 4,
-  H3C_ERROR_FRAME_INCOMPLETE = 5,
-  H3C_ERROR_FRAME_MALFORMED = 6
+
+  // HTTP/3 connection errors
+
+  H3C_ERROR_FRAME_MALFORMED = 0x0100,
+
+  // HTTP/3 stream errors
+
+  // h3c library errors
+
+  // QUIC's error codes are limited to 16 bytes which leaves all numbers larger
+  // than 16 bytes for library error codes.
+
+  H3C_ERROR_INTERNAL = UINT16_MAX + 1,
+  H3C_ERROR_BUF_TOO_SMALL,
+  H3C_ERROR_VARINT_OVERFLOW,
+  H3C_ERROR_SETTING_OVERFLOW,
+  H3C_ERROR_FRAME_INCOMPLETE
 } H3C_ERROR;
 
 H3C_EXPORT const char *h3c_strerror(H3C_ERROR error);
--- h3c/src/error.c
+++ h3c/src/error.c
@@ -4,19 +4,25 @@ const char *h3c_strerror(H3C_ERROR error)
 {
   switch (error) {
   case H3C_SUCCESS:
-    return "success";
-  case H3C_ERROR_INTERNAL:
-    return "internal error";
-  case H3C_ERROR_BUF_TOO_SMALL:
-    return "buf too small";
-  case H3C_ERROR_VARINT_OVERFLOW:
-    return "varint overflow";
-  case H3C_ERROR_SETTING_OVERFLOW:
-    return "setting overflow";
-  case H3C_ERROR_FRAME_INCOMPLETE:
-    return "frame incomplete";
+    return "h3c: success";
+
+  // HTTP/3 connection errors
+
   case H3C_ERROR_FRAME_MALFORMED:
-    return "frame malformed";
+    return "HTTP/3: malformed frame";
+
+  // h3c library errors
+
+  case H3C_ERROR_INTERNAL:
+    return "h3c: internal error";
+  case H3C_ERROR_BUF_TOO_SMALL:
+    return "h3c: buf too small";
+  case H3C_ERROR_VARINT_OVERFLOW:
+    return "h3c: varint overflow";
+  case H3C_ERROR_SETTING_OVERFLOW:
+    return "h3c: setting overflow";
+  case H3C_ERROR_FRAME_INCOMPLETE:
+    return "h3c: frame incomplete";
   }
 
   return "unknown error";
