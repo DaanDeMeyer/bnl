1cc1ef8a0233968ac6ae294cf7a895fcb1f497a5
Daan De Meyer <daan.j.demeyer@gmail.com>
2019-05-11 22:50:13 +0200

    Move logging macros from log.h to util.h.
    The macros make too many assumptions so its better to not expose them.

--- h3c/include/h3c/log.h
+++ h3c/include/h3c/log.h
@@ -23,21 +23,6 @@ typedef struct h3c_log_t {
   void *context;
 } h3c_log_t;
 
-#define H3C_LOG(level, format, ...)                                            \
-  if (log && log->log) {                                                       \
-    log->log(log->context, level, __FILE__, __func__, __LINE__, format,        \
-             __VA_ARGS__);                                                     \
-  }                                                                            \
-  (void) 0
-
-// clang-format off
-#define H3C_LOG_TRACE(format, ...)   H3C_LOG(H3C_LOG_LEVEL_TRACE,   format, __VA_ARGS__)
-#define H3C_LOG_DEBUG(format, ...)   H3C_LOG(H3C_LOG_LEVEL_DEBUG,   format, __VA_ARGS__)
-#define H3C_LOG_INFO(format, ...)    H3C_LOG(H3C_LOG_LEVEL_INFO,    format, __VA_ARGS__)
-#define H3C_LOG_WARNING(format, ...) H3C_LOG(H3C_LOG_LEVEL_WARNING, format, __VA_ARGS__)
-#define H3C_LOG_ERROR(format, ...)   H3C_LOG(H3C_LOG_LEVEL_ERROR,   format, __VA_ARGS__)
-// clang-format on
-
 #ifdef __cplusplus
 }
 #endif
--- h3c/src/util.h
+++ h3c/src/util.h
@@ -6,6 +6,21 @@
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
+#define H3C_LOG(level, format, ...)                                            \
+  if (log && log->log) {                                                       \
+    log->log(log->context, level, __FILE__, __func__, __LINE__, format,        \
+             __VA_ARGS__);                                                     \
+  }                                                                            \
+  (void) 0
+
+// clang-format off
+#define H3C_LOG_TRACE(format, ...)   H3C_LOG(H3C_LOG_LEVEL_TRACE,   format, __VA_ARGS__)
+#define H3C_LOG_DEBUG(format, ...)   H3C_LOG(H3C_LOG_LEVEL_DEBUG,   format, __VA_ARGS__)
+#define H3C_LOG_INFO(format, ...)    H3C_LOG(H3C_LOG_LEVEL_INFO,    format, __VA_ARGS__)
+#define H3C_LOG_WARNING(format, ...) H3C_LOG(H3C_LOG_LEVEL_WARNING, format, __VA_ARGS__)
+#define H3C_LOG_ERROR(format, ...)   H3C_LOG(H3C_LOG_LEVEL_ERROR,   format, __VA_ARGS__)
+// clang-format on
+
 #define H3C_ERROR(error)                                                       \
   switch ((error)) {                                                           \
     case H3C_SUCCESS:                                                          \
