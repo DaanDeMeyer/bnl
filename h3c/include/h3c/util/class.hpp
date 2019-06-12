9c3b8e287125c3921f9f725f354668019c67767d
Daan De Meyer <daan.j.demeyer@gmail.com>
2019-06-12 13:56:36 +0200

    Add extra utility macros to `util/class.hpp`
    These are useful to reduce boilerplate even when `H3C_MOVE_ONLY` is not
    applicable.

--- h3c/include/h3c/util/class.hpp
+++ h3c/include/h3c/util/class.hpp
@@ -6,3 +6,19 @@
                                                                                \
   name(name &&) = default;           /* NOLINT */                              \
   name &operator=(name &&) = default /* NOLINT */
+
+#define H3C_DEFAULT_COPY(name)                                                 \
+  name(const name &) = default;           /* NOLINT */                         \
+  name &operator=(const name &) = default /* NOLINT */
+
+#define H3C_NO_COPY(name)                                                      \
+  name(const name &) = delete;           /* NOLINT */                          \
+  name &operator=(const name &) = delete /* NOLINT */
+
+#define H3C_DEFAULT_MOVE(name)                                                 \
+  name(name &&) = default;           /* NOLINT */                              \
+  name &operator=(name &&) = default /* NOLINT */
+
+#define H3C_NO_MOVE(name)                                                      \
+  name(name &&) = delete;           /* NOLINT */                               \
+  name &operator=(name &&) = delete /* NOLINT */
