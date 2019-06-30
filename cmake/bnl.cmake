a275dacfa6c16dfd75fad5d087cf87b27eb58562
Daan De Meyer <daan.j.demeyer@gmail.com>
2019-06-30 14:41:44 +0200

    Make cddm bnl-specific.
    The benefits of a shared CMake library do not offset the added
    complexity. Making cddm bnl-specific reduces the learning curve
    of the CMake build system.

--- cmake/bnl.cmake
+++ cmake/bnl.cmake
@@ -1,105 +1,55 @@
-# CDDM (CMake Daan De Meyer)
-# Version: v0.0.20
-#
-# Description: Encapsulates common CMake configuration for cross-platform
-# C/C++ libraries.
-#
-# Features:
-# - Warnings
-#   - UNIX: -Wall, -Wextra, ...
-#   - Windows: /W4
-# - Sanitizers (optional, UNIX only)
-# - clang-tidy (optional)
-# - Automatic installation including pkg-config and CMake config files.
-# - Automatic export header generation.
-#
-# Options:
-#
-# Every option is prefixed with the upper cased project name. For example, if
-# the project is named reproc every option is prefixed with `REPROC_`.
-#
-# Installation options:
-# - `INSTALL`: Generate installation rules (default: `ON` unless
-#   `BUILD_SHARED_LIBS` is false and the project is built via
-#   `add_subdirectory`).
-# - `INSTALL_CMAKECONFIGDIR`: CMake config files installation directory
-#   (default: `${CMAKE_INSTALL_LIBDIR}/cmake`).
-# - `INSTALL_PKGCONFIG`: Install pkg-config files (default: `ON`)
-# - `INSTALL_PKGCONFIGDIR`: pkg-config files installation directory
-#   (default: `${CMAKE_INSTALL_LIBDIR}/pkgconfig`).
-#
-# Developer options:
-# - `SANITIZERS`: Build with sanitizers (default: `OFF`).
-# - `TIDY`: Run clang-tidy when building (default: `OFF`).
-# - `WARNINGS_AS_ERRORS`: Add -Werror or equivalent to the compile flags and
-#   clang-tidy (default: `OFF`).
-#
-# Functions:
-# - `cddm_add_common`
-# - `cddm_add_library`
-#
-# See https://github.com/DaanDeMeyer/reproc for an example on how to use cddm.
-#
-# NOTE: All languages used have to be enabled before including cddm.
-
-# CMake 3.13 added target_link_options.
-cmake_minimum_required(VERSION 3.13)
-
-set(PNL ${PROJECT_NAME}) # PROJECT_NAME_LOWER (PNL)
-string(TOUPPER ${PROJECT_NAME} PNU) # PROJECT_NAME_UPPER (PNU)
-
 ### Installation options ###
 
-get_directory_property(CDDM_IS_SUBDIRECTORY PARENT_DIRECTORY)
+get_directory_property(BNL_IS_SUBDIRECTORY PARENT_DIRECTORY)
 
 # Don't add libraries to the install target by default if the project is built
 # from within another project as a static library.
-if(CDDM_IS_SUBDIRECTORY AND NOT BUILD_SHARED_LIBS)
-  option(${PNU}_INSTALL "Generate installation rules." OFF)
+if(BNL_IS_SUBDIRECTORY AND NOT BUILD_SHARED_LIBS)
+  option(BNL_INSTALL "Generate installation rules." OFF)
 else()
-  option(${PNU}_INSTALL "Generate installation rules." ON)
+  option(BNL_INSTALL "Generate installation rules." ON)
 endif()
 
-mark_as_advanced(${PNU}_INSTALL)
+mark_as_advanced(BNL_INSTALL)
 
 include(GNUInstallDirs)
 
-option(${PNU}_INSTALL_PKGCONFIG "Install pkg-config files." ON)
+option(BNL_INSTALL_PKGCONFIG "Install pkg-config files." ON)
 
-set(${PNU}_INSTALL_CMAKECONFIGDIR ${CMAKE_INSTALL_LIBDIR}/cmake
+set(BNL_INSTALL_CMAKECONFIGDIR ${CMAKE_INSTALL_LIBDIR}/cmake
     CACHE STRING "CMake config files installation directory.")
-set(${PNU}_INSTALL_PKGCONFIGDIR ${CMAKE_INSTALL_LIBDIR}/pkgconfig
+set(BNL_INSTALL_PKGCONFIGDIR ${CMAKE_INSTALL_LIBDIR}/pkgconfig
     CACHE STRING "pkg-config files installation directory.")
 
 mark_as_advanced(
-  ${PNU}_INSTALL
-  ${PNU}_INSTALL_PKGCONFIG
-  ${PNU}_INSTALL_CMAKECONFIGDIR
-  ${PNU}_INSTALL_PKGCONFIGDIR
+  BNL_INSTALL
+  BNL_INSTALL_PKGCONFIG
+  BNL_INSTALL_CMAKECONFIGDIR
+  BNL_INSTALL_PKGCONFIGDIR
 )
 
 ### Developer options ###
 
-option(${PNU}_TIDY "Run clang-tidy when building.")
-option(${PNU}_SANITIZERS "Build with sanitizers.")
-option(${PNU}_WARNINGS_AS_ERRORS "Add -Werror or equivalent to the compile flags and clang-tidy.")
+option(BNL_TIDY "Run clang-tidy when building.")
+option(BNL_SANITIZERS "Build with sanitizers.")
+option(BNL_WARNINGS_AS_ERRORS "Add -Werror or equivalent to the compile flags and clang-tidy.")
 
 mark_as_advanced(
-  ${PNU}_TIDY
-  ${PNU}_SANITIZERS
-  ${PNU}_WARNINGS_AS_ERRORS
+  BNL_TIDY
+  BNL_SANITIZERS
+  BNL_WARNINGS_AS_ERRORS
 )
 
 ### clang-tidy ###
 
-if(${PNU}_TIDY)
-  find_program(CDDM_CLANG_TIDY_PROGRAM clang-tidy)
-  mark_as_advanced(CDDM_CLANG_TIDY_PROGRAM)
+if(BNL_TIDY)
+  find_program(BNL_CLANG_TIDY_PROGRAM clang-tidy)
+  mark_as_advanced(BNL_CLANG_TIDY_PROGRAM)
 
-  if(CDDM_CLANG_TIDY_PROGRAM)
-    if(${PNU}_WARNINGS_AS_ERRORS)
-      set(CDDM_CLANG_TIDY_PROGRAM
-          ${CDDM_CLANG_TIDY_PROGRAM} -warnings-as-errors=*)
+  if(BNL_CLANG_TIDY_PROGRAM)
+    if(BNL_WARNINGS_AS_ERRORS)
+      set(BNL_CLANG_TIDY_PROGRAM
+          ${BNL_CLANG_TIDY_PROGRAM} -warnings-as-errors=*)
     endif()
   else()
     message(FATAL_ERROR "clang-tidy not found")
@@ -120,10 +70,10 @@ if(MSVC)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")
 
   include(CheckCXXCompilerFlag)
-  check_cxx_compiler_flag(/permissive- CDDM_HAVE_PERMISSIVE)
+  check_cxx_compiler_flag(/permissive- BNL_HAVE_PERMISSIVE)
 endif()
 
-if(${PNU}_SANITIZERS)
+if(BNL_SANITIZERS)
   if(MSVC)
     message(FATAL_ERROR "Building with sanitizers is not supported when using the Visual C++ toolchain.")
   endif()
@@ -143,7 +93,7 @@ include(CMakePackageConfigHelpers)
 
 # Applies common configuration to `TARGET`. `OUTPUT_DIRECTORY` specifies the
 # target's output directory.
-function(cddm_add_common TARGET OUTPUT_DIRECTORY)
+function(bnl_add_common TARGET OUTPUT_DIRECTORY)
   target_compile_features(${TARGET} PUBLIC cxx_std_11)
 
   set_target_properties(${TARGET} PROPERTIES
@@ -152,11 +102,11 @@ function(cddm_add_common TARGET OUTPUT_DIRECTORY)
     LIBRARY_OUTPUT_DIRECTORY "${OUTPUT_DIRECTORY}"
   )
 
-  if(${PNU}_TIDY AND CDDM_CLANG_TIDY_PROGRAM)
+  if(BNL_TIDY AND BNL_CLANG_TIDY_PROGRAM)
     set_target_properties(${TARGET} PROPERTIES
       # CLANG_TIDY_PROGRAM is a list so we surround it with quotes to pass it as
       # a single argument.
-      CXX_CLANG_TIDY "${CDDM_CLANG_TIDY_PROGRAM}"
+      CXX_CLANG_TIDY "${BNL_CLANG_TIDY_PROGRAM}"
     )
   endif()
 
@@ -166,11 +116,11 @@ function(cddm_add_common TARGET OUTPUT_DIRECTORY)
     target_compile_options(${TARGET} PRIVATE
       /nologo # Silence MSVC compiler version output.
       /wd4068 # Allow unknown pragmas.
-      # We assume the cddm library is compiled with the same compiler and
-      # runtime used to compile the client code.
+      # We assume the library is compiled with the same compiler and runtime
+      # used to compile the client code.
       /wd4251
-      $<$<BOOL:${${PNU}_WARNINGS_AS_ERRORS}>:/WX> # -Werror
-      $<$<BOOL:${CDDM_HAVE_PERMISSIVE}>:/permissive->
+      $<$<BOOL:${BNL_WARNINGS_AS_ERRORS}>:/WX> # -Werror
+      $<$<BOOL:${BNL_HAVE_PERMISSIVE}>:/permissive->
     )
 
     target_link_options(${TARGET} PRIVATE
@@ -186,12 +136,12 @@ function(cddm_add_common TARGET OUTPUT_DIRECTORY)
       -Wconversion
       -Wsign-conversion
       -Wno-unknown-pragmas
-      $<$<BOOL:${${PNU}_WARNINGS_AS_ERRORS}>:-Werror>
-      $<$<BOOL:${${PNU}_WARNINGS_AS_ERRORS}>:-pedantic-errors>
+      $<$<BOOL:${BNL_WARNINGS_AS_ERRORS}>:-Werror>
+      $<$<BOOL:${BNL_WARNINGS_AS_ERRORS}>:-pedantic-errors>
     )
   endif()
 
-  if(${PNU}_SANITIZERS)
+  if(BNL_SANITIZERS)
     target_compile_options(${TARGET} PRIVATE
       -fsanitize=address,undefined
     )
@@ -208,13 +158,13 @@ function(cddm_add_common TARGET OUTPUT_DIRECTORY)
   )
 endfunction()
 
-# Adds a new library with name `${PNL}-${NAME}` and applies common
+# Adds a new library with name `bnl-${NAME}` and applies common
 # configuration to it.
-function(cddm_add_library NAME)
-  set(TARGET ${PNL}-${NAME})
+function(bnl_add_library NAME)
+  set(TARGET bnl-${NAME})
   add_library(${TARGET})
 
-  cddm_add_common(${TARGET} lib)
+  bnl_add_common(${TARGET} lib)
   # Enable -fvisibility=hidden and -fvisibility-inlines-hidden (if applicable).
   set_target_properties(${TARGET} PROPERTIES
     CXX_VISIBILITY_PRESET hidden
@@ -229,8 +179,8 @@ function(cddm_add_library NAME)
   # the export header without requiring the user to add a #define which we want
   # to avoid.
   generate_export_header(${TARGET}
-    BASE_NAME ${PNU}_${NAME_UPPER}
-    EXPORT_FILE_NAME ${CMAKE_CURRENT_BINARY_DIR}/include/${PNL}/${NAME}/export.hpp
+    BASE_NAME BNL_${NAME_UPPER}
+    EXPORT_FILE_NAME ${CMAKE_CURRENT_BINARY_DIR}/include/bnl/${NAME}/export.hpp
   )
 
   # Make sure we follow the popular naming convention for shared libraries on
@@ -251,7 +201,7 @@ function(cddm_add_library NAME)
   # Adapted from https://codingnest.com/basic-cmake-part-2/.
   # Each library is installed separately (with separate config files).
 
-  if(${PNU}_INSTALL)
+  if(BNL_INSTALL)
 
     ## Config files
 
@@ -262,12 +212,12 @@ function(cddm_add_library NAME)
       ## Headers
 
       install(
-        DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/include/${PNL}
+        DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/include/bnl
         DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
       )
 
       install(
-        DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/include/${PNL}
+        DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/include/bnl
         DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
       )
 
@@ -288,7 +238,7 @@ function(cddm_add_library NAME)
       install(
         EXPORT ${TARGET}-targets
         FILE ${TARGET}-targets.cmake
-        DESTINATION ${${PNU}_INSTALL_CMAKECONFIGDIR}/${TARGET}
+        DESTINATION ${BNL_INSTALL_CMAKECONFIGDIR}/${TARGET}
       )
 
       write_basic_package_version_file(
@@ -301,7 +251,7 @@ function(cddm_add_library NAME)
           ${CMAKE_CURRENT_SOURCE_DIR}/${NAME}-config.cmake.in
           ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}-config.cmake
         INSTALL_DESTINATION
-          ${${PNU}_INSTALL_CMAKECONFIGDIR}/${TARGET}
+          ${BNL_INSTALL_CMAKECONFIGDIR}/${TARGET}
       )
 
       install(
@@ -309,13 +259,13 @@ function(cddm_add_library NAME)
           ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}-config.cmake
           ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}-config-version.cmake
         DESTINATION
-          ${${PNU}_INSTALL_CMAKECONFIGDIR}/${TARGET}
+          ${BNL_INSTALL_CMAKECONFIGDIR}/${TARGET}
       )
     endif()
 
     # pkg-config
 
-    if(${PNU}_INSTALL_PKGCONFIG AND EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${NAME}.pc.in)
+    if(BNL_INSTALL_PKGCONFIG AND EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${NAME}.pc.in)
       configure_file(
         ${CMAKE_CURRENT_SOURCE_DIR}/${NAME}.pc.in
         ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.pc
@@ -324,7 +274,7 @@ function(cddm_add_library NAME)
 
       install(
         FILES ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.pc
-        DESTINATION ${${PNU}_INSTALL_PKGCONFIGDIR}
+        DESTINATION ${BNL_INSTALL_PKGCONFIGDIR}
       )
     endif()
   endif()
