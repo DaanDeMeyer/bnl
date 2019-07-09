a51fd3369f26501e373f15c47a380546b3636aab
Daan De Meyer <daan.j.demeyer@gmail.com>
2019-07-09 18:54:07 +0200

    Make `buffer_view` and `buffer_view_mut` default constructible.

--- bnl/base/include/bnl/base/buffer_view.hpp
+++ bnl/base/include/bnl/base/buffer_view.hpp
@@ -10,12 +10,13 @@ namespace base {
 
 class BNL_BASE_EXPORT buffer_view {
 public:
+  buffer_view() = default;
+  buffer_view(const uint8_t *data, size_t size) noexcept;
+
   template <size_t Size>
   buffer_view(const char (&data)[Size]) noexcept // NOLINT
       : buffer_view(reinterpret_cast<const uint8_t *>(data), Size - 1)
   {}
-
-  buffer_view(const uint8_t *data, size_t size) noexcept;
 
   const uint8_t *data() const noexcept;
 
@@ -26,8 +27,8 @@ public:
   const uint8_t *end() const noexcept;
 
 private:
-  const uint8_t *data_;
-  size_t size_;
+  const uint8_t *data_ = nullptr;
+  size_t size_ = 0;
 };
 
 BNL_BASE_EXPORT bool operator==(buffer_view lhs, buffer_view rhs) noexcept;
@@ -35,6 +36,7 @@ BNL_BASE_EXPORT bool operator!=(buffer_view lhs, buffer_view rhs) noexcept;
 
 class BNL_BASE_EXPORT buffer_view_mut {
 public:
+  buffer_view_mut() = default;
   buffer_view_mut(uint8_t *data, size_t size) noexcept;
 
   uint8_t *data() noexcept;
@@ -48,8 +50,8 @@ public:
   operator buffer_view() const noexcept; // NOLINT
 
 private:
-  uint8_t *data_;
-  size_t size_;
+  uint8_t *data_ = nullptr;
+  size_t size_ = 0;
 };
 
 } // namespace base
