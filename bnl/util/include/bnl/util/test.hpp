eba85c5cf64659c1923d365595aed2fdb74696c4
Daan De Meyer <daan.j.demeyer@gmail.com>
2019-07-16 22:16:39 +0200

    Move value from result in `extract`.

--- bnl/util/include/bnl/util/test.hpp
+++ bnl/util/include/bnl/util/test.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
+#include <utility>
+
 template<typename E>
 auto
 extract(E expression) -> typename decltype(expression())::value_type
 {
   auto result = expression();
   REQUIRE(result);
-  return result.value();
+  return std::move(result).value();
 }
 
 #define EXTRACT(...) extract([&]() { return __VA_ARGS__; });
